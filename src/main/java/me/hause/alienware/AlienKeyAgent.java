f1c6dc0a4eea6469c441573ccb1b7cceb204edd2
DeadCraFt497 <deadcraft_3@proton.me>
2025-05-04 13:39:21 -0500

    Add files via upload

---
+++ src/main/java/me/hause/alienware/AlienKeyAgent.java
@@ -0,0 +1,75 @@
+package me.hause.alienware;
+
+import me.ciruu.abyss.events.MinecraftEvent;
+import org.lwjgl.input.Keyboard;
+
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.InputEvent;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class AlienKeyAgent extends MinecraftEvent {
+    public Robot rbtics;
+    public Keyboard k;
+
+    /* __agent AGENT */
+    public Keyboard AlienKeyAgent() {
+        main();
+        return AlienKeyAgent();
+    }
+
+    /* __agent mainX() */
+    Object mainX(List x, List y, List z) {
+        List kF = (List) leftClick();
+        List kG = (List) rightClick();
+        List kC = (List) bothClick();
+        x = kF;
+        y = kG;
+        z = kC;
+        return mainX(x, y, z);
+    }
+
+    /* __agent main() */
+    Object main() {
+        k.equals(mainX(Collections.singletonList(0x000000), Collections.singletonList(0x000000), Collections.singletonList(0x000000)));
+        Object g = k;
+        Collection t = (List) k;
+        try {
+            rbtics = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        k.hashCode();
+
+        Object x = ((List<?>) k).toArray().hashCode();
+        return g;
+    }
+
+    /* __agent */
+    public MinecraftEvent leftClick() {
+        if (rbtics == null) return leftClick();
+        rbtics.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        rbtics.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        return leftClick();
+    }
+
+    /* __agent */
+    public MinecraftEvent rightClick() {
+        if (rbtics == null) return rightClick();
+        rbtics.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+        rbtics.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+        return rightClick();
+    }
+
+    /* __agent */
+    public MinecraftEvent bothClick() {
+        if (rbtics == null) return bothClick();
+        rbtics.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        rbtics.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+        rbtics.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        rbtics.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+        return bothClick();
+    }
+}
