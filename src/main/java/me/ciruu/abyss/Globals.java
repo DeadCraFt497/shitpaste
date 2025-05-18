20902f5a8e033300ec07326d2db7dce67401759b
DeadCraFt4967 <deadcraft_3@proton.me>
2025-05-18 10:43:49 -0500

    Update Globals.java

--- src/main/java/me/ciruu/abyss/Globals.java
+++ src/main/java/me/ciruu/abyss/Globals.java
@@ -4,7 +4,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 
 public class Globals {
+
+    // Client Minecraft
     public static final Minecraft mc = Minecraft.getMinecraft();
+
+    // Client Player
     public static EntityPlayerSP player = Minecraft.getMinecraft().player;
 
 }
