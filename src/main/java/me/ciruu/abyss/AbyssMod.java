1a041cb90ffc2ad9dd085106f3d9afa385a48f5d
DeadCraFt4967 <deadcraft_3@proton.me>
2025-05-18 10:35:51 -0500

    Update AbyssMod.java

--- src/main/java/me/ciruu/abyss/AbyssMod.java
+++ src/main/java/me/ciruu/abyss/AbyssMod.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 
 @Mod(modid="abyss", name="Abyss", version="1.6.0")
 public class AbyssMod {
+    
     @Mod.Instance(value="abyss")
     public static AbyssMod INSTANCE;
     public static Manager manager;
