548c7f544076bc38c6a2787f99bd331bd8788b71
DeadCraFt4967 <deadcraft_3@proton.me>
2025-05-18 10:35:18 -0500

    Update AbyssMod.java
    Context

--- src/main/java/me/ciruu/abyss/AbyssMod.java
+++ src/main/java/me/ciruu/abyss/AbyssMod.java
@@ -1,3 +1,4 @@
+/** Game Mod Abyss v201 */
 package me.ciruu.abyss;
 
 import me.zero.alpine.bus.EventBus;
@@ -22,25 +23,32 @@ public class AbyssMod {
     public static final String VERSION = "1.6.0";
     public static final String suffix = "\u1d00\u0299\u028f\ua731\ua731";
 
-    public CheckGuard[] Abyss;
+    /** @Info Check That The Player Is Aloud To Use This! */
+    public CheckGuard Abyss = new CheckGuard();
     public static EventBus EVENT_BUS;
-    public static Logger[] log;
+    public static Logger log = new Logger();
 
+    /** @Info ForgeMCModLoaderPre */
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent fMLPreInitializationEvent) {
+        manager.preinit(fMLPreInitialization);
     }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent fMLInitializationEvent) {
-        /*MinecraftForge.EVENT_BUS.register((Object)new Class489());*/
-    }
-
+    
+    /** @Info ForgeMCModLoaderPost */
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent fMLPostInitializationEvent) {
-
+        manager.postinit(fMLPostInitialization);
     }
 
+    /** @Info ForgeMCModLoaderInit */
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent fMLInitializationEvent) {
+        manager.init(fMLInitializationEvent);
+    }
+
+    /** @Info Modification Module's Load */
     static {
         manager = new Manager();
     }
 }
+
